22d14abde88f54621d8fab000fd414603b0c1c48
Alfonso Camacho+Pardo+Filho <acamachopardofilho@groupon.com>
2020-07-13 17:35:39 +0100

    drop - initial commiut

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# smomt
+# Smart Store ?
+
+## Features
+
+- [x] Run in a worker
+- [x] postMessage pass action and selector result
+- [ ] Support for vanilla, react and preact
+- [x] implement selector
+- [ ] implement selector cache
+- [x] implement effects
+- [ ] single or multiple stores
+- [x] combine reducers
+- [x] combine effects
+- [x] combine selectors
+- [ ] communication between stores
+- [ ] channel communication
+- [ ] lazy-load/increment store
---
+++ index.mjs
@@ -0,0 +1,253 @@
+
+class SmartStore {
+    constructor({ name, initialState, reducers, effects, selectors, options }) {
+        this.worker = new Worker(this.createCode(initialState, reducers, selectors, effects), { name })
+        this.reducers = reducers
+        this.effects = effects
+        this.select = this.makeSelectors(selectors)
+        this.options = options
+        this.pending = {}
+        //this.cache
+
+        this.worker.onmessage = (e) => {
+            // console.log('worker response', e.data)
+
+            // console.log('pending', this.pending)
+
+            this.pending[e.data.id](e.data.projection)
+        }
+    }
+
+    static combineSelectors(...args) {
+
+        const lastIndex = args.length - 1
+        const selectors = args.slice(0, lastIndex).map(fn => fn.name)
+        const fn = args[lastIndex].toString()
+
+        return [...selectors, fn]
+    }
+
+    getId() {
+        return Math.random().toString(32).slice(2, 10)
+    }
+
+    makeSelectors(selectors) {
+        const select = {};
+        for (const selector in selectors) {
+            select[selector] = (...args) => new Promise((resolve, reject) => {
+                const id = this.getId()
+                this.worker.postMessage({ cmd: 'SELECT', selector, args, id })
+
+                this.pending[id] = (data) => {
+                    resolve(data)
+                    delete this.pending[id]
+                }
+            })
+
+        }
+        return select
+    }
+
+    dispatch(msg) {
+        this.worker.postMessage({ cmd: 'DISPATCH', payload: msg })
+    }
+
+    convertSelectorToString(selectors) {
+        let sel = ''
+
+        for (let selector in selectors) {
+            if (typeof selectors[selector] === 'function') {
+                sel += `${selector}: ${selectors[selector].toString()},`
+            }
+
+            if (Array.isArray(selectors[selector])) {
+                const current = selectors[selector]
+                const lastIndex = current.length - 1
+                const selectorNames = current.slice(0, lastIndex)
+                const fn = current[lastIndex].toString()
+
+                sel += `${selector}: (state) => {
+                    const sel = ${JSON.stringify(selectorNames)}
+                    const opts = sel.map((name) => selectors[name](state))
+                    const fn = ${fn}
+                    return fn.apply(null, opts)
+                }`
+            }
+        }
+
+        return `{ ${sel} }`;
+    }
+
+    convertReducersToString(reducers) {
+        let str = ''
+
+        for (const reducer of reducers) {
+            str += reducer.toString() + ','
+        }
+
+        return `[${str}]`
+    }
+
+    convertEffectsToString() {
+        return '[]'
+    }
+
+    createCode(initialState, reducers, selectors, effects) {
+
+        const selectorsObj = this.convertSelectorToString(selectors)
+        const reducersObj = this.convertReducersToString(reducers)
+        const effectsObj = this.convertReducersToString(effects)
+
+        const workerCode = `
+
+        let state = ${ JSON.stringify(initialState)}
+
+        
+        function reducer(action, state, deps) {
+            const reducers = ${ reducersObj}
+
+            return reducers.reduce((updatedState, fn) => fn(action, updatedState, deps), state)
+        }
+
+        function effect(action, state, deps, select) {
+            const effects = ${ effectsObj}
+            
+            const bindSelectors = Object.keys(select).reduce((acc, it) => {
+                acc[it] = select[it].bind(null, state)
+                return acc;
+            }, {})
+
+            for(const _effect of effects) {
+                _effect(action, state, deps, bindSelectors)
+            }
+
+            //console.log('effestcs', effects, select)
+        }
+
+        self.onmessage = (e) => {
+            //console.log('log', e.data)
+
+            const selectors = ${ selectorsObj}
+
+            switch(e.data.cmd) {
+                case 'DISPATCH': {
+                    state = reducer(e.data.payload, state)
+                    effect(e.data.payload, state, {}, selectors)
+                    // console.log('state?', state)
+                    break;
+                }
+                case 'SELECT': {
+                    
+
+                    let projection = selectors[e.data.selector](state)
+                    if(typeof projection === 'function') {
+                        projection = projection.apply(null, e.data.args)
+                    }
+                    self.postMessage({projection, id: e.data.id})
+                
+                    break;
+                }
+            }
+        }`
+
+        const blob = new Blob([workerCode], { type: 'application/javascript' })
+        const code = URL.createObjectURL(blob)
+
+        return code
+
+    }
+}
+
+
+
+
+// ----------------------------------
+
+
+function reducerA(action, state, deps) {
+    switch (action.type) {
+        case 'INC':
+            return { total: state.total + 1 }
+
+        case 'DEC':
+            return { total: state.total - 1 }
+
+        default:
+            return { ...state }
+    }
+}
+
+
+function reducerB(action, state, deps) {
+    // console.log('reducer B', action, state, deps)
+    switch (action.type) {
+        case 'INC':
+            return { total: state.total + 1 }
+
+        case 'DEC':
+            return { total: state.total - 1 }
+
+        default:
+            return { ...state }
+    }
+}
+
+async function effectA(action, state) {
+    if (action.type === 'FETCH') {
+        const r = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+
+        // console.log('response', r)
+    }
+}
+
+async function effectB(action, state, deps, select) {
+    if (action.type === 'FETCH') {
+        const r = await fetch('https://jsonplaceholder.typicode.com/todos/2')
+            .then(response => response.json())
+
+        // console.log('??', select)
+        console.log('response (', r.title + ') : total = ' + select.getStringify())
+    }
+}
+
+
+const getTotal = (state) => state?.total
+const getDouble = (state) => state?.total * 2
+const getSuffix = (state) => (txt) => state?.total + txt
+const getStringify = SmartStore.combineSelectors(getTotal, getDouble, (total, double) => 'Result = ' + (total + double))
+
+async function Program() {
+    const store = new SmartStore({ name: 'Test', initialState: { total: 0 }, reducers: [reducerA, reducerB], effects: [effectA, effectB], selectors: { getTotal, getDouble, getSuffix, getStringify } })
+    const store2 = new SmartStore({ name: 'Test2', initialState: { total: 0 }, reducers: [reducerA, reducerB], effects: [effectA, effectB], selectors: { getTotal, getDouble, getSuffix, getStringify } })
+
+    console.log('store instance', store)
+
+    console.log('R: total', await store.select.getTotal())
+    store.dispatch({ type: 'INC' })
+    store.dispatch({ type: 'INC' })
+    store.dispatch({ type: 'INC' })
+
+    console.log('R: total', await store.select.getTotal())
+
+    store.dispatch({ type: 'DEC' })
+    console.log('R: total', await store.select.getTotal())
+    console.log('R: getDouble', await store.select.getDouble())
+    // store.select.getDouble()
+    // store.select.getSuffix(' === total')
+    console.log('R: getSuffix', await store.select.getSuffix(' === total'))
+    console.log('R: getStringify', await store.select.getStringify())
+    // store.select.getSuffix(' === total')
+    // store.select.getStringify()
+    store.dispatch({ type: 'FETCH' })
+
+    
+    store2.dispatch({ type: 'INC' })
+    store2.dispatch({ type: 'INC' })
+    store2.dispatch({ type: 'INC' })
+    console.log('R2: total', await store2.select.getTotal())
+
+
+}
+
+Program()
